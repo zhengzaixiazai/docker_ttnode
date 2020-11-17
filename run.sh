204f38a11b077635566d7139a828ea0c8cea7fdf
zhengzaixiazai <zhengzaixiazai@gmail.com>
2020-11-18 00:54:47 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 docker run -itd \
   -v /mnt/data/ttnode:/mnts \
   --name ttnode \
-  --net=macnet --ip=192.168.2.2 --dns=114.114.114.114 --mac-address C2:F2:9C:C5:B2:94 \
+  --net=macnet --ip=192.168.7.1 --dns=114.114.114.114 --mac-address C2:F2:9C:C5:B2:94 \
   --privileged=true \
   --restart=always \
   ericwang2006/ttnode
